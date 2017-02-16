caee6348e94beca78a68d43e9698d6f67cc2f8e3
Muhammad Khattab <msskmohammed96@gmail.com>
2017-02-16 18:12:18 +0200

    added commands script

---
+++ commands.sh
@@ -0,0 +1,23 @@
+#assemble bootload
+nasm bootload.asm
+
+# image file of a floppy disk
+dd if=/dev/zero of=floppya.img bs=512 count=2880
+
+# copy bootload to the beginning of floppya.img
+dd if=bootload of=floppya.img bs=512 count=1 conv=notrunc
+
+# compile kernel.c
+bcc -ansi -c -o kernel
+
+# assemble kernel.asm
+as86 kernel.asm -o kernel_asm.o
+
+# link kernel.c with kernel.o
+ld86 -o kernel -d kernel.o kernel_asm.o
+
+# copy kernel.c to floppya.img at sector 3
+dd if=kernel of=floppya.img bs=512 conv=notrunc seek=3
+
+# excute
+chmod +x commands.sh
